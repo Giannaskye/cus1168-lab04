1f8a3d98d4eb84f6f82abda809f2cb01e544a7c7
“Your <giannaskye1@gmail.com>
2025-02-24 17:41:51 -0500

    finished homework

--- src/main/java/academy/javapro/ExpressionParser.java
+++ src/main/java/academy/javapro/ExpressionParser.java
@@ -11,13 +11,20 @@ class ExpressionParser {
 
     // expr → expr + term
     public double parseExpression() {
+
         // TODO: Get the leftmost term value by calling parseTerm()
         // TODO: While we haven't reached the end of input and current char is '+'
         // TODO: Increment position to skip the '+' operator
         // TODO: Get the next term on the right
         // TODO: Add the right term to our running total
         // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseExpression");
+        double leftMost = parseTerm();
+        while (position < input.length() && input.charAt(position) == '+') {
+            position++;
+            double rightTerm = parseTerm();
+            leftMost += rightTerm;
+        }
+        return leftMost;
     }
 
     // term → term * factor
@@ -28,38 +35,63 @@ class ExpressionParser {
         // TODO: Get the next factor on the right
         // TODO: Multiply the right factor with our running total
         // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseTerm");
+        double leftMost = parseFactor();
+        while (position < input.length() && input.charAt(position) == '*') {
+            position++;
+            double nextFactor = parseFactor();
+            leftMost *= nextFactor;
+        }
+        return leftMost;
     }
 
     // factor → ( expr )
     private double parseFactor() {
-        // TODO: Check if we have an opening parenthesis and haven't reached end of input
+        // TODO: Check if we have an opening parenthesis and haven't reached end of
+        // input
         // TODO: Increment position to skip the opening parenthesis
         // TODO: Parse the expression inside the parentheses
         // TODO: Increment position to skip the closing parenthesis
         // TODO: Return the result of the parenthesized expression
         // TODO: If no parentheses, parse and return a number
-        throw new UnsupportedOperationException("Implement parseFactor");
+
+        if (position < input.length() && input.charAt(position) == '(') {
+            position++;
+            double num = parseExpression();
+            position++;
+            return num;
+        } else {
+            return parseNumber();
+        }
+
     }
 
     // Parse a numeric value
     private double parseNumber() {
+
         // TODO: Create a StringBuilder to collect digits
-        // TODO: While we haven't reached the end and current char is digit or decimal point
+        // TODO: While we haven't reached the end and current char is digit or decimal
+        // point
         // TODO: Append the current digit to our number string
         // TODO: Move to next character
         // TODO: Convert the string of digits to a double and return it
-        throw new UnsupportedOperationException("Implement parseNumber");
+        StringBuilder sd = new StringBuilder();
+        while (position < input.length() && Character.isDigit(input.charAt(position))
+                || input.charAt(position) == '.') {
+            sd.append(input.charAt(position));
+            position++;
+        }
+        return Double.parseDouble(sd.toString());
+
     }
 
     public static void main(String[] args) {
         // Test cases
         String[] testCases = {
-                "2 + 3 * (4 + 5)",    // Complex expression with parentheses
-                "2 + 3 * 4",          // Basic arithmetic with precedence
-                "(2 + 3) * 4",        // Parentheses changing precedence
+                "2 + 3 * (4 + 5)", // Complex expression with parentheses
+                "2 + 3 * 4", // Basic arithmetic with precedence
+                "(2 + 3) * 4", // Parentheses changing precedence
                 "2 * (3 + 4) * (5 + 6)", // Multiple parentheses
-                "1.5 + 2.5 * 3"       // Decimal numbers
+                "1.5 + 2.5 * 3" // Decimal numbers
         };
 
         // Process each test case
@@ -74,4 +106,5 @@ class ExpressionParser {
             }
         }
     }
+
 }
